92aeb417a0f9f8df8967cb621bb70ad4fa0a32e1
Liovirgilda Mendonca <liovirgilda_developer@gmail.com>
2022-10-29 19:42:22 +0100

    create env.py with secret_key and configure flask- sqlalchemy

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+from os import path
+if path.exists("env.py"):
+    import env
 
+# create the app
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY") 
+
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.db"
+
+# create the extension 
+db = SQLAlchemy()
+
+# initialize the app with the extension
+db.init_app(app)
 
 @app.route("/")
 def homepage():
