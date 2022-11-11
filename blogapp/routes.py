861fd85e355938158884c37fd738e4957afe98f3
Liovirgilda Mendonca <liovirgilda.developer@gmail.com>
2022-11-11 20:07:05 +0000

    Sign up page and route

--- blogapp/routes.py
+++ blogapp/routes.py
@@ -1,35 +1,42 @@
 from flask import render_template, request, redirect, url_for, flash
 from blogapp import app, db, bcrypt
-from blogapp.models import User, Post
-import sqlite3
-from sqlite3 import IntegrityError
+from blogapp.models import User
+
 
 @app.route("/")
 def homepage():
     return render_template("homepage.html", title="Blog Homepage")
 
-@app.route("/login", methods = ['GET','POST'])
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     return render_template("login.html", title="login")
 
-@app.route("/signup", methods = ['GET','POST'])
+
+@app.route("/signup", methods=['GET', 'POST'])
 def signup():
     if request.method == "POST":
-        usernameExist = db.session.execute(db.select(User).where(User.username == request.form["username"] )).scalar()
-        if usernameExist:
-            flash("Username already exists")
+        if request.form["password"] != request.form["confPassword"]:
+            flash("Passwords don't match")
         else:
-            emailExist = db.session.execute(db.select(User).where(User.email == request.form["email"] )).scalar()
-            if emailExist:
-                flash("Email already exists")
+            usernameExist = db.session.execute(db.select(User).where(
+                User.username == request.form["username"])).scalar()
+            if usernameExist:
+                flash("Username already exists")
             else:
-                user = User(
-                    name = request.form["name"],
-                    username = request.form["username"],
-                    email = request.form["email"],
-                    password = bcrypt.generate_password_hash(request.form["password"]).decode('utf-8')
-                )
-                db.session.add(user)
-                db.session.commit() 
-                return redirect(url_for('homepage', id=user.id))
-    return render_template("signup.html", title="login")
+                emailExist = db.session.execute(db.select(User).where(
+                    User.email == request.form["email"])).scalar()
+                if emailExist:
+                    flash("Email already exists")
+                else:
+                    user = User(
+                        name=request.form["name"],
+                        username=request.form["username"],
+                        email=request.form["email"],
+                        password=bcrypt.generate_password_hash(
+                            request.form["password"]).decode('utf-8')
+                    )
+                    db.session.add(user)
+                    db.session.commit()
+                    return redirect(url_for('homepage', id=user.id))
+    return render_template("signup.html", title="signup")
